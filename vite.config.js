c822350230cecbf256a1f1b0f0735aef2355b1e9
misteltein <itogahibiki@gmail.com>
2024-04-05 16:00:27 +0900

    test

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,9 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     vue(),
   ],
